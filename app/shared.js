589ceaad37fd96ec0426aae724a9f865b589e9be
unknown <svc-jenkins@ST-BUILD-001.coexist.local>
2015-07-16 15:11:22 -0700

    commit from gulp!

--- app/shared.js
+++ app/shared.js
@@ -4,7 +4,7 @@ define(["require", "exports"], function (require, exports) {
         function logger() {
         }
         logger.log = function (message) {
-            // we need a change here so the deploy doesnt fail ... yet again (again)  asdf...
+            // hopefully this will work
             console.log(message);
         };
         return logger;
